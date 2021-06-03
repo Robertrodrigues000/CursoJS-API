2396e5a2fd83761877d2bc02e4b7053c69a68784
Robert Rodrigues <robertrodrigues000@hotmail.com>
2021-06-03 13:49:14 -0300

    update API

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno'
-
 class HomeController {
   async index(req, res) {
-  const novoAluno = await Aluno.create({
-    nome: 'Marley',
-    sobrenome: 'Joao',
-    email: 'jijih@gmail.com',
-    idade: 80,
-    peso: 39,
-    altura: 120,
-  });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
